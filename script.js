23656bb6c7a50a90384cd3a54e8c1bdebf0d053b
David Wissocq <dwissocq@gmail.com>
2021-04-13 10:04:43 +0200

    fix date

--- script.js
+++ script.js
@@ -28,58 +28,24 @@ console.log(nav[0]);
 
 // Dernière mesure nav[0]
 nav[0].addEventListener("click", function () {
-        fetch(baseApiUrl + '/last?measure-type=' + select.value).then(function (response) {
-            response.json().then(function (result) {
-                console.log(result);
-
-
-                //supprimer le bloc résultat s'il existe
-                let deleteResult = document.getElementById("result");
-                if (deleteResult.firstElementChild !== null) {
-                    deleteResult.removeChild(deleteResult.firstElementChild);
-                }
-                // créer un bloc avec les résultats (h3+h4)
-                let blocResult = document.createElement("div");
-
-                // Demander de l'aide à Jules pour bien afficher la date
-                let h3 = document.createElement("h3");
-                h3.textContent = nav[0].textContent + " du " + result.measureDate;
-                blocResult.appendChild(h3);
-
-                let h4 = document.createElement("h4");
-                h4.textContent = result.type + " : " + result.value + " " + result.unit
-                blocResult.appendChild(h4);
-
-                document.getElementById("result").appendChild(blocResult);
-
-
-            });
-        }).catch(function (error) {
-            console.log('Il y a eu un problème avec la récupération de la dernière mesure ' + error.message);
-        });
-
-    });
-
-
-    // TOP mesures
-
-nav[1].addEventListener("click", function () {
-    fetch(baseApiUrl + '/top?measure-type=' + select.value).then(function (response) {
+    fetch(baseApiUrl + '/last?measure-type=' + select.value).then(function (response) {
         response.json().then(function (result) {
             console.log(result);
 
-
-            //supprimer le bloc résultat s'il existe
+            // Supprimer le bloc résultat s'il existe
             let deleteResult = document.getElementById("result");
             if (deleteResult.firstElementChild !== null) {
                 deleteResult.removeChild(deleteResult.firstElementChild);
             }
-            // créer un bloc avec les résultats (h3+h4)
+            // Créer un bloc avec les résultats (h3+h4)
             let blocResult = document.createElement("div");
 
             // Demander de l'aide à Jules pour bien afficher la date
             let h3 = document.createElement("h3");
-            h3.textContent = nav[1].textContent + " du " + result.measureDate;
+            let measureDate = new Date(result.measureDate);
+            h3.textContent = nav[0].textContent + " du " + measureDate.getDate()
+                + "/" + measureDate.getMonth()
+                + "/" + measureDate.getFullYear()
             blocResult.appendChild(h3);
 
             let h4 = document.createElement("h4");
@@ -88,12 +54,47 @@ nav[1].addEventListener("click", function () {
 
             document.getElementById("result").appendChild(blocResult);
 
+        });
+    }).catch(function (error) {
+        console.log('Il y a eu un problème avec la récupération de la dernière mesure ' + error.message);
+    });
+});
+
+
+// TOP mesures
+
+nav[1].addEventListener("click", function () {
+    fetch(baseApiUrl + '/top?measure-type=' + select.value).then(function (response) {
+        response.json().then(function (result) {
+            console.log(result);
+
+
+            // Supprimer le bloc résultat s'il existe
+            let deleteResult = document.getElementById("result");
+            if (deleteResult.firstElementChild !== null) {
+                deleteResult.removeChild(deleteResult.firstElementChild);
+            }
+            // Créer un bloc avec les résultats (h3+h4)
+            let blocResult = document.createElement("div");
+
+            // Demander de l'aide à Jules pour bien afficher la date
+            let h3 = document.createElement("h3");
+            let measureDate = new Date(result.measureDate);
+            h3.textContent = nav[1].textContent + " du " + measureDate.getDate()
+                + "/" + measureDate.getMonth()
+                + "/" + measureDate.getFullYear()
+            blocResult.appendChild(h3);
+
+            let h4 = document.createElement("h4");
+            h4.textContent = result.type + " : " + result.value + " " + result.unit
+            blocResult.appendChild(h4);
+
+            document.getElementById("result").appendChild(blocResult);
 
         });
     }).catch(function (error) {
         console.log('Il y a eu un problème avec la récupération de la dernière mesure ' + error.message);
     });
-
 });
 
 // Tableaux de mesures
@@ -104,7 +105,7 @@ nav[2].addEventListener("click", function () {
     // ajouter le cadre pour choisir les dates
 
 
-   fetch(baseApiUrl + '/measure-type=TEMPERATURE&start-date=11%2F04%2F2021&end-date=12%2F04%2F2021').then(function (response) {
+    fetch(baseApiUrl + '?measure-type=TEMPERATURE&start-date=2021-04-12T12%3A00&end-date=2021-04-13T12%3A00').then(function (response) {
         response.json().then(function (result) {
             console.log(result);
         });
@@ -114,8 +115,6 @@ nav[2].addEventListener("click", function () {
     });
 
 });
-
-
 
 // recupérer les données
 
