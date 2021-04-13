5d3c0ac489789ece536ed045d82d3c5219162f5f
David Wissocq <dwissocq@gmail.com>
2021-04-13 12:47:26 +0200

    Tables en cours

--- script.js
+++ script.js
@@ -91,31 +91,31 @@ nav[2].addEventListener("click", function () {
 
     // ajouter le cadre pour choisir les dates
     let inputDateStart = document.createElement("input");
-    inputDateStart.type="datetime-local";
-    inputDateStart.id ="Start";
-
-    // let labelSD=document.createElement ("label");
-    // labelSD.value="Start Date";
-    // inputDateStart.appendChild(labelSD);
+    inputDateStart.type = "datetime-local";
+    inputDateStart.id = "Start";
+    let labelSD = document.createElement("label");
+    labelSD.textContent = "Date de début";
     let inputDateEnd = document.createElement("input");
-    inputDateEnd.type="datetime-local";
-    inputDateStart.id="End";
-
+    inputDateEnd.type = "datetime-local";
+    inputDateEnd.id = "End";
+    let labelED = document.createElement("label");
+    labelED.textContent = "Date de fin";
 
 
     // Vérifier si le cadre des dates existe
-        let dateChoice = document.getElementById("date");
-        if (dateChoice.firstElementChild === null) {
+    let dateChoice = document.getElementById("date");
+    if (dateChoice.firstElementChild === null) {
 
+        dateChoice.appendChild(labelSD);
         dateChoice.appendChild(inputDateStart);
+        dateChoice.appendChild(labelED);
         dateChoice.appendChild(inputDateEnd);
     }
-   let  dateStartChoice=document.getElementById("Start");
-        let dateEndChoice=document.getElementById("End");
+    let dateStartChoice = document.getElementById("Start");
+    let dateEndChoice = document.getElementById("End");
 
-
-        // Afficher la date en console
-    if (inputDateStart.value !== null){
+    // Afficher la date en console (test)
+    if (inputDateStart.value !== null) {
         console.log(document.getElementById("Start").value);
         console.log(document.getElementById("End").value);
     }
@@ -123,7 +123,7 @@ nav[2].addEventListener("click", function () {
 // Selection des dates par l'utilisateur
 
 // Récupération des données sur le serveur
-    fetch(baseApiUrl + '?measure-type='+ select.value + '&start-date='+ dateStartChoice.value +'&end-date='+dateEndChoice.value).then(function (response) {
+    fetch(baseApiUrl + '?measure-type=' + select.value + '&start-date=' + dateStartChoice.value + '&end-date=' + dateEndChoice.value).then(function (response) {
         response.json().then(function (result) {
             console.log(result);
         });
@@ -133,23 +133,48 @@ nav[2].addEventListener("click", function () {
         if (deleteResult.firstElementChild !== null) {
             deleteResult.removeChild(deleteResult.firstElementChild);
         }
-        // afficher le tableau sur la page
-        // Créer un bloc avec les résultats (h3+h4)
-        let blocResult = document.createElement("div");
+        // Créer le tableau sur la page
+
+        let tableResult = document.createElement("table");
+        let tableHead = document.createElement("thead");
+
+        let headRow = document.createElement("tr");
+        document.getElementById("result").appendChild(headRow);
+
+        let th1 = document.createElement("th");
+        th1.textContent = "Date";
+        headRow.appendChild(th1);
+
+        let th2 = document.createElement("th");
+        th2.textContent = "Valeurs de mesure";
+        headRow.appendChild(th2);
+
+        let newRow = document.createElement("tr")
+        document.getElementById("result").appendChild(newRow);
+
+        let td1 = document.createElement("td");
+        td1.textContent = "test 1";
+        newRow.appendChild(td1);
+
+        let td2 = document.createElement("td");
+        td2.textContent = "test 2";
+        newRow.appendChild(td2);
+
+
 
         // Affichage - à modifier
-        let h3 = document.createElement("h3");
-        let measureDate = new Date(result.measureDate);
-        h3.textContent = nav[1].textContent + " du " + measureDate.getDate() // à noter la possibilité d'utiliser la fonction .slice (1,10)
-            + "/" + measureDate.getMonth()
-            + "/" + measureDate.getFullYear()
-        blocResult.appendChild(h3);
-
-        let h4 = document.createElement("h4");
-        h4.textContent = result.type + " : " + result.value + " " + result.unit
-        blocResult.appendChild(h4);
-
-        document.getElementById("result").appendChild(blocResult);
+        // let h3 = document.createElement("h3");
+        // let measureDate = new Date(result.measureDate);
+        // h3.textContent = nav[1].textContent + " du " + measureDate.getDate() // à noter la possibilité d'utiliser la fonction .slice (1,10)
+        //     + "/" + measureDate.getMonth()
+        //     + "/" + measureDate.getFullYear()
+        // blocResult.appendChild(h3);
+        //
+        // let h4 = document.createElement("h4");
+        // h4.textContent = result.type + " : " + result.value + " " + result.unit
+        // blocResult.appendChild(h4);
+        //
+        // document.getElementById("result").appendChild(blocResult);
 
 
     }).catch(function (error) {
