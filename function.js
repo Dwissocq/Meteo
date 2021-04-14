166d59a7a01212d75d473e0bc3a689beaed0d921
David Wissocq <dwissocq@gmail.com>
2021-04-14 12:45:07 +0200

    version finale !

--- function.js
+++ function.js
@@ -5,3 +5,53 @@ const deleteResultInDOM = function () {
         deleteResult.removeChild(deleteResult.firstElementChild);
     }
 };
+
+// Création du bloc résultat, choix du last ou top mesure en argument
+const createResult = function (result,a) {
+
+    // Création d'une section DIV pour regrouper les deux titres h3 et h4  en un seul bloc supprimable
+    let blocResult = document.createElement("div");
+    let h3 = document.createElement("h3");
+    let measureDate = new Date(result.measureDate);
+    h3.textContent = document.getElementById(a).textContent + " du " + measureDate.getDate()
+        + "/" + (measureDate.getMonth()+1)
+        + "/" + measureDate.getFullYear()
+    blocResult.appendChild(h3);
+
+    let h4 = document.createElement("h4");
+    h4.textContent = result.type + " : " + result.value + " " + result.unit
+    blocResult.appendChild(h4);
+
+    document.getElementById("result").appendChild(blocResult);
+}
+
+// Création du bloc calendrier
+const createCalendar = function (){
+
+    let inputDateStart = document.createElement("input");
+    inputDateStart.type = "datetime-local";
+    inputDateStart.id = "Start";
+    let labelSD = document.createElement("label");
+    labelSD.textContent = "Date de début";
+    let inputDateEnd = document.createElement("input");
+    inputDateEnd.type = "datetime-local";
+    inputDateEnd.id = "End";
+    inputDateEnd.min = inputDateStart.value;
+    let labelED = document.createElement("label");
+    labelED.textContent = "Date de fin";
+
+    // Initialisation avec une date par défaut basée sur today (date du jour)
+    let today = new Date()
+    inputDateStart.value = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + today.getDate() + "T00:00"
+    inputDateEnd.value = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + today.getDate() + "T23:59"
+
+    // Vérifier si le cadre des dates existe, affichage si aucun cadre
+    let dateChoice = document.getElementById("date");
+    if (dateChoice.firstElementChild === null) {
+
+        dateChoice.appendChild(labelSD);
+        dateChoice.appendChild(inputDateStart);
+        dateChoice.appendChild(labelED);
+        dateChoice.appendChild(inputDateEnd);
+    }
+}
--- script.js
+++ script.js
@@ -9,22 +9,9 @@ document.getElementById("lastMeasure").addEventListener("click", function () {
 
             // Supprimer le bloc résultat s'il existe
             deleteResultInDOM();
-            // Créer un bloc avec les résultats (h3+h4)
-            let blocResult = document.createElement("div");
 
-            // Demander de l'aide à Jules pour bien afficher la date
-            let h3 = document.createElement("h3");
-            let measureDate = new Date(result.measureDate);
-            h3.textContent = document.getElementById("lastMeasure").textContent + " du " + measureDate.getDate()
-                + "/" + measureDate.getMonth()
-                + "/" + measureDate.getFullYear()
-            blocResult.appendChild(h3);
-
-            let h4 = document.createElement("h4");
-            h4.textContent = result.type + " : " + result.value + " " + result.unit
-            blocResult.appendChild(h4);
-
-            document.getElementById("result").appendChild(blocResult);
+            // Affichage
+            createResult(result,"lastMeasure");
 
         });
     }).catch(function (error) {
@@ -32,33 +19,17 @@ document.getElementById("lastMeasure").addEventListener("click", function () {
     });
 });
 
-
 // TOP mesures
 
 document.getElementById("topMeasure").addEventListener("click", function () {
     fetch(baseApiUrl + '/top?measure-type=' + select.value).then(function (response) {
         response.json().then(function (result) {
-            // console.log(result);
-
 
             // Supprimer le bloc résultat s'il existe
             deleteResultInDOM();
-            // Créer un bloc avec les résultats (h3+h4)
-            let blocResult = document.createElement("div");
 
             // Affichage
-            let h3 = document.createElement("h3");
-            let measureDate = new Date(result.measureDate);
-            h3.textContent = document.getElementById("topMeasure").textContent + " du " + measureDate.getDate() // à noter la possibilité d'utiliser la fonction .slice (1,10)
-                + "/" + measureDate.getMonth()
-                + "/" + measureDate.getFullYear()
-            blocResult.appendChild(h3);
-
-            let h4 = document.createElement("h4");
-            h4.textContent = result.type + " : " + result.value + " " + result.unit
-            blocResult.appendChild(h4);
-
-            document.getElementById("result").appendChild(blocResult);
+            createResult(result,"topMeasure");
 
         });
     }).catch(function (error) {
@@ -70,42 +41,13 @@ document.getElementById("topMeasure").addEventListener("click", function () {
 
 document.getElementById("tableMeasure").addEventListener("click", function () {
 
-    // ajouter le cadre pour choisir les dates
-    let today = new Date()
-    let inputDateStart = document.createElement("input");
-    inputDateStart.type = "datetime-local";
-    inputDateStart.id = "Start";
-    inputDateStart.value = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + today.getDate() + "T00:00"
-    let labelSD = document.createElement("label");
-    labelSD.textContent = "Date de début";
-    let inputDateEnd = document.createElement("input");
-    inputDateEnd.type = "datetime-local";
-    inputDateEnd.id = "End";
-    inputDateEnd.value = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + today.getDate() + "T23:59"
-    inputDateEnd.min = inputDateStart.value;
-    let labelED = document.createElement("label");
-    labelED.textContent = "Date de fin";
+    createCalendar();
 
-
-    // Vérifier si le cadre des dates existe
-    let dateChoice = document.getElementById("date");
-    if (dateChoice.firstElementChild === null) {
-
-        dateChoice.appendChild(labelSD);
-        dateChoice.appendChild(inputDateStart);
-        dateChoice.appendChild(labelED);
-        dateChoice.appendChild(inputDateEnd);
-    }
-    let dateStartChoice = document.getElementById("Start");
-    let dateEndChoice = document.getElementById("End");
-
-    // Afficher la date en console (test)
-    if (inputDateStart.value !== null) {
-        // console.log(document.getElementById("Start").value);
-        // console.log(document.getElementById("End").value);
-    }
     // Supprimer le bloc résultat s'il existe
     deleteResultInDOM();
+
+    let dateStartChoice = document.getElementById("Start");
+    let dateEndChoice = document.getElementById("End");
 
     // Créer le tableau vide sur la page
     let tableResult = document.createElement("table");
@@ -126,11 +68,9 @@ document.getElementById("tableMeasure").addEventListener("click", function () {
     th2.textContent = "Valeur de mesure";
     headRow.appendChild(th2);
 
-
     // Création des données du tableau
     let tableBody = document.createElement("tbody");
     tableResult.appendChild(tableBody);
-
 
 // Récupération des données sur le serveur
     fetch(baseApiUrl + '?measure-type=' + select.value + '&start-date=' + dateStartChoice.value + '&end-date=' + dateEndChoice.value).then(function (response) {
@@ -138,9 +78,7 @@ document.getElementById("tableMeasure").addEventListener("click", function () {
             for (let i = 0; i < result.length; i++) {
                 let measureDate = new Date(result[i].measureDate);
                 let date = measureDate.getDate() + "/" + measureDate.getMonth() + "/" + measureDate.getFullYear() + " " + measureDate.getHours() + ":" + measureDate.getMinutes() + ":" + measureDate.getSeconds();
-                // console.log(date);
                 let value = result[i].value + " " + result[i].unit;
-                // console.log(value);
                 let newRow = document.createElement("tr")
                 let td1 = document.createElement("td");
                 td1.textContent = date;
@@ -153,52 +91,23 @@ document.getElementById("tableMeasure").addEventListener("click", function () {
             }
         });
 
-
     }).catch(function (error) {
         console.log('Il y a eu un problème avec la récupération de la dernière mesure ' + error.message);
     });
 
 });
 
-
 // Graph de mesures
 
 document.getElementById("chartMeasure").addEventListener("click", function () {
 
-    // ajouter le cadre pour choisir les dates
-    let today = new Date()
-    let inputDateStart = document.createElement("input");
-    inputDateStart.type = "datetime-local";
-    inputDateStart.id = "Start";
-    //Date par défaut
-    inputDateStart.value = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + today.getDate() + "T00:00"
-    let labelSD = document.createElement("label");
-    labelSD.textContent = "Date de début";
-    let inputDateEnd = document.createElement("input");
-    inputDateEnd.type = "datetime-local";
-    inputDateEnd.id = "End";
-    //Date par défaut
-    inputDateEnd.value = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + today.getDate() + "T23:59"
-    inputDateEnd.min = inputDateStart.value;
-    let labelED = document.createElement("label");
-    labelED.textContent = "Date de fin";
-
-
-    // Vérifier si le cadre des dates existe
-    let dateChoice = document.getElementById("date");
-    if (dateChoice.firstElementChild === null) {
-
-        dateChoice.appendChild(labelSD);
-        dateChoice.appendChild(inputDateStart);
-        dateChoice.appendChild(labelED);
-        dateChoice.appendChild(inputDateEnd);
-    }
-    let dateStartChoice = document.getElementById("Start");
-    let dateEndChoice = document.getElementById("End");
-
+    createCalendar();
 
     // Supprimer le bloc résultat s'il existe
     deleteResultInDOM();
+
+    let dateStartChoice = document.getElementById("Start");
+    let dateEndChoice = document.getElementById("End");
 
     // Création de la zone de graphique
     let graphResult = document.createElement("canvas");
@@ -209,7 +118,7 @@ document.getElementById("chartMeasure").addEventListener("click", function () {
     let graphData = new Array();
     let unit = "";
 
-// Récupération des données sur le serveur ***
+// Récupération des données sur le serveur
     fetch(baseApiUrl + '?measure-type=' + select.value + '&start-date=' + dateStartChoice.value + '&end-date=' + dateEndChoice.value).then(function (response) {
         response.json().then(function (result) {
             unit = result[0].unit
@@ -219,11 +128,8 @@ document.getElementById("chartMeasure").addEventListener("click", function () {
                 graphDate.push(measureDate);
                 graphData.push(value);
             }
-            console.log(graphDate);
-            console.log(graphData);
 
-
-            //Création du graphique ***
+            //Création du graphique
             var ctx = document.getElementById("myChart").getContext('2d');
             var myChart = new Chart(ctx, {
                 type: 'line',
@@ -241,24 +147,10 @@ document.getElementById("chartMeasure").addEventListener("click", function () {
                         borderWidth: 1
                     }]
                 },
-                options: {
-                    scales: {
-                        y: {
-                            min: 17,
-                            max: 22,
-                            beginAtZero: false
-                        }
-                    }
-                }
             });
         });
 
     }).catch(function (error) {
         console.log('Il y a eu un problème avec la récupération de la dernière mesure ' + error.message);
     });
-
 });
-
-
-
-
